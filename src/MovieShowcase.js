60d79a163c8af2cff9dc1a2782fbfa9160bfe166
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-18 07:43:27 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,7 +4,7 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
-  generateMovieCards = () => {
+let generateMovieCards = () => {
    return movieData.map((movie, index) => <MovieCard
 		      key={index}
 		      title={movie.title}
