a3317f82e7f55455a32b28a5f169f623251b463a
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-18 07:40:27 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -36,8 +36,6 @@ export default class MovieCard extends Component {
 
   render() {
     return (
-       <div className="movie-card">
-        {/* which component should receive which props? */}
         <CardFront poster={posterMap[this.props.poster]} />
         <CardBack title={this.props.title}
 					IMDBRating={this.props.IMDBRating}
